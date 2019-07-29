d693e051e2e0ededed9ee8b8e901f6875d92b16b
Simon Johnston <johnstonskj@gmail.com>
2019-07-29 11:23:49 -0700

    restructured to only create a provider if necessary

--- src/bin/folio.rs
+++ src/bin/folio.rs
@@ -32,90 +32,100 @@ fn main() {
     } else {
         let default_currency = with_code("USD").unwrap();
 
-        let provider = match IEXProvider::new() {
-            Ok(provider) => provider,
-            Err(RequestError::ConfigurationError(err)) => {
-                println!("Error configuring provider: {}", err);
-                return ();
-            }
-            Err(err) => {
-                println!("Unknown error from provider: {:?}", err);
-                return ();
-            }
-        };
-
         if let Some(portfolio) = get_portfolio() {
             match cmd {
-                Command::Show => show::show_portfolio(portfolio, provider),
-                Command::Watch => watch::watch_portfolio(portfolio),
-                Command::Add(s, p, q) => {
-                    let p = match p {
-                        Some(p) => {
-                            let ps: Vec<&str> = p.split(".").collect();
-                            if ps.len() == 1 {
-                                Money::of_major_minor(
-                                    portfolio.default_currency.unwrap_or(default_currency),
-                                    ps.get(0).unwrap().parse::<i32>().unwrap(),
-                                    0)
-                            } else if ps.len() == 2 {
-                                Money::of_major_minor(
-                                    portfolio.default_currency.unwrap_or(default_currency),
-                                    ps.get(0).unwrap().parse::<i32>().unwrap(),
-                                    ps.get(1).unwrap().parse::<i32>().unwrap())
-                            } else {
-                                println!("Could not parse price {}", p);
-                                Money::zero(default_currency)
-                            }
-                        },
-                        None => Money::zero(default_currency)
-                    };
-                    let q = match q {
-                        Some(q) => match q.parse::<u32>() {
-                            Ok(n) => n,
-                            Err(_) => {
-                                println!("Could not parse quantity {}", q);
-                                0
-                            },
+                Command::Show | Command::Watch => {
+                    let provider = match IEXProvider::new() {
+                        Ok(provider) => provider,
+                        Err(RequestError::ConfigurationError(err)) => {
+                            println!("Error configuring provider: {}", err);
+                            return ();
                         }
-                        None => 0,
-                    };
-                    let new_item = Item::Price(
-                        s,
-                        Holding {
-                            purchase_price: p,
-                            quantity: q,
-                        }
-                    );
-                    let new_portfolio = Portfolio {
-                        default_currency: portfolio.default_currency,
-                        items: portfolio.items.into_iter().chain(vec![new_item]).collect()
-                    };
-                    match model::write_file(None, &new_portfolio) {
                         Err(err) => {
-                            println!("Failed to save portfolio file, error: {:?}", err);
-                            ()
-                        },
-                        Ok(_) => (),
+                            println!("Unknown error from provider: {:?}", err);
+                            return ();
+                        }
+                    };
+
+                    match cmd {
+                        Command::Show => show::show_portfolio(portfolio, provider),
+                        Command::Watch => watch::watch_portfolio(portfolio),
+                        _ => (),
                     }
                 },
-                Command::Remove(symbol) => {
-                    let new_portfolio = Portfolio {
-                        default_currency: portfolio.default_currency,
-                        items: portfolio.items.iter().filter(|item|
-                            match item {
-                                Item::Watch(s) | Item::Price(s, _) => *s != symbol,
+                Command::Add(_, _, _) | Command::Remove(_) => {
+                    match cmd {
+                        Command::Add(s, p, q) => {
+                            let p = match p {
+                                Some(p) => {
+                                    let ps: Vec<&str> = p.split(".").collect();
+                                    if ps.len() == 1 {
+                                        Money::of_major_minor(
+                                            portfolio.default_currency.unwrap_or(default_currency),
+                                            ps.get(0).unwrap().parse::<i32>().unwrap(),
+                                            0)
+                                    } else if ps.len() == 2 {
+                                        Money::of_major_minor(
+                                            portfolio.default_currency.unwrap_or(default_currency),
+                                            ps.get(0).unwrap().parse::<i32>().unwrap(),
+                                            ps.get(1).unwrap().parse::<i32>().unwrap())
+                                    } else {
+                                        println!("Could not parse price {}", p);
+                                        Money::zero(default_currency)
+                                    }
+                                },
+                                None => Money::zero(default_currency)
+                            };
+                            let q = match q {
+                                Some(q) => match q.parse::<u32>() {
+                                    Ok(n) => n,
+                                    Err(_) => {
+                                        println!("Could not parse quantity {}", q);
+                                        0
+                                    },
+                                }
+                                None => 0,
+                            };
+                            let new_item = Item::Price(
+                                s,
+                                Holding {
+                                    purchase_price: p,
+                                    quantity: q,
+                                }
+                            );
+                            let new_portfolio = Portfolio {
+                                default_currency: portfolio.default_currency,
+                                items: portfolio.items.into_iter().chain(vec![new_item]).collect()
+                            };
+                            match model::write_file(None, &new_portfolio) {
+                                Err(err) => {
+                                    println!("Failed to save portfolio file, error: {:?}", err);
+                                    ()
+                                },
+                                Ok(_) => (),
                             }
-                        ).cloned().collect()
-                    };
-                    match model::write_file(None, &new_portfolio) {
-                        Err(err) => {
-                            println!("Failed to save portfolio file, error: {:?}", err);
-                            ()
                         },
-                        Ok(_) => (),
+                        Command::Remove(symbol) => {
+                            let new_portfolio = Portfolio {
+                                default_currency: portfolio.default_currency,
+                                items: portfolio.items.iter().filter(|item|
+                                    match item {
+                                        Item::Watch(s) | Item::Price(s, _) => *s != symbol,
+                                    }
+                                ).cloned().collect()
+                            };
+                            match model::write_file(None, &new_portfolio) {
+                                Err(err) => {
+                                    println!("Failed to save portfolio file, error: {:?}", err);
+                                    ()
+                                },
+                                Ok(_) => (),
+                            }
+                        },
+                        _ => (),
                     }
                 },
-                Command::None => ()
+                Command::None => (),
             }
         }
     }
